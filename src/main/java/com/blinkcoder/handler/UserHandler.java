0be9a02c620fea96d833906ced815c987457aef6
张艺辰 <yidongnan@gmail.com>
2014-05-08 10:23:31 +0800

    处理空指针异常

--- src/main/java/com/blinkcoder/handler/UserHandler.java
+++ src/main/java/com/blinkcoder/handler/UserHandler.java
@@ -45,7 +45,7 @@ public class UserHandler extends Handler {
                         int id = Integer.parseInt(fieldArray[0]);
                         String openid = fieldArray[1];
                         User user = User.dao.Get(id);
-                        if (user.get("openid").equals(openid)) {
+                        if (openid.equals(user.get("openid"))) {
                             request.setAttribute("g_user", user);
                         }
                     }
